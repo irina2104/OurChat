e1de116dd34d90bcfe8dc62c0702ff8850a19139
Ana-Irina MĂGUREANU (42425) <ana_irina.magureanu@stud.etti.upb.ro>
2018-08-12 20:17:05 +0300

    first commit

---
+++ app/src/main/java/ourchat/com/ourchat/UserDetails.java
@@ -0,0 +1,9 @@
+package ourchat.com.ourchat;
+
+public class UserDetails {
+
+        public static String username = "";
+        static String password = "";
+        public static String chatWith = "";
+
+}
